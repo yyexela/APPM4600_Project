f83db55d5f4d37a19edffd74ca6e42ec1ad17f6b
Tyler <tyje9409@colorado.edu>
2022-11-06 00:08:02 -0600

    More testing of Ridge

--- Code/ridge_2a_driver.py
+++ Code/ridge_2a_driver.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import estimators 
 from sample import random_sample
+from sklearn.linear_model import Ridge
 
 #no interval is given of where to sample f, choose [-5, 5)
 f = lambda x: 3*x + 2
@@ -14,34 +15,50 @@ seed = 50
 #get random sample, and divide into training and validation data
 train_x, train_y, valid_x, valid_y = random_sample(number_of_samples, f, a, b, number_of_train_samples, seed = seed)
 
+fig_sample, ax_sample = plt.subplots(1,1)
+fig_sample.set_size_inches(6,6)
+xeval = np.linspace(-5,5,1000)
+ax_sample.plot(train_x, train_y, 'o', label = 'Train Data')
+ax_sample.plot(valid_x, valid_y, 'o', label = 'Validation Data')
+ax_sample.plot(xeval, f(xeval), label = 'f(x) = 3x + 2')
+ax_sample.set_title('Train and Validation Data')
+ax_sample.set_xlabel('x')
+ax_sample.set_ylabel('y')
+ax_sample.legend()
+fig_sample.savefig('2a_sample_data.png')
+plt.show()
+
 
 degree = 1
-gammas_initial = [0,0.1]
+gammas_initial = [0,10]
 
-fig_initial, ax_initial = plt.subplots(1,2)
 rss_initial = []
 xeval = np.linspace(-5,5,1000)
 for gamma in gammas_initial:
     ridge = estimators.ridge(gamma, degree)
     ridge.fit(train_x, train_y)
     rss_initial.append(ridge.RSS(valid_x, valid_y))
-    predict_y = ridge.predict(valid_x)
-    ax_initial[0].plot(valid_x, predict_y, label = 'gamma = ' + str(gamma))
-ax_initial[0].plot(xeval, f(xeval), label = 'f(x) = 3x + 2')
-ax_initial[0].plot(valid_x, valid_y, '.', label = 'Validation Data')
-ax_initial[0].legend()
-ax_initial[0].set_title('Predicted lines for gamma = 0, 0.1')
-ax_initial[0].set_xlabel('x')
-ax_initial[0].set_ylabel('y')
-ax_initial[1].plot(gammas_initial, rss_initial)
-ax_initial[1].set_title('Residual Sum of Squares for gamma = 0, 0.1')
-ax_initial[1].set_xlabel('gamma')
-ax_initial[1].set_ylabel('Residual Sum of Squares')
+    predict = ridge.predict(valid_x)
+    print(predict)
 
-plt.show()
+print (rss_initial)
+
+sklearn_0 = Ridge(0)
+sklearn_0.fit(train_x.reshape(-1,1), train_y)
+predict_sk_0 = sklearn_0.predict(valid_x.reshape(-1,1))
+
+sklearn_1 = Ridge(10)
+sklearn_1.fit(train_x.reshape(-1,1), train_y)
+predict_sk_1 = sklearn_1.predict(valid_x.reshape(-1,1))
+
+print(predict_sk_0)
+print(predict_sk_1)
 
 
-gammas_log = [0, 0.1, 1, 10, 100]
+
+
+
+gammas_log = np.linspace(0,1000,1000)
 fig_log10, ax_log10 = plt.subplots(1,1)
 rss_log10 = []
 for gamma in gammas_log:
@@ -55,7 +72,7 @@ ax_log10.set_xlabel('gamma')
 ax_log10.set_ylabel('Residual Sum of Squares')
 plt.show()
 
-gammas_fine = np.linspace(0,10,100)
+gammas_fine = np.linspace(-5,10,200)
 fig_fine, ax_fine = plt.subplots(1,1)
 rss_fine = []
 for gamma in gammas_fine:
