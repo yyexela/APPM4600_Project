7aac1632171e6c605909ed9aae16db4dfd0299ca
Tyler <tyje9409@colorado.edu>
2022-11-09 15:46:36 -0700

    Made Nicer Graphs

--- Code/ridge_2a_driver.py
+++ Code/ridge_2a_driver.py
@@ -15,33 +15,22 @@ seed = 50
 train_x, train_y, valid_x, valid_y = random_sample(number_of_samples, f, a, b, number_of_train_samples, seed = seed)
 
 
+xeval = np.linspace(-5,5,1000)
 degree = 1
 gammas_initial = [0,0.1]
-
-fig_initial, ax_initial = plt.subplots(1,2)
+fig_initial, ax_initial = plt.subplots(1,1)
 rss_initial = []
-xeval = np.linspace(-5,5,1000)
 for gamma in gammas_initial:
     ridge = estimators.ridge(gamma, degree)
     ridge.fit(train_x, train_y)
     rss_initial.append(ridge.RSS(valid_x, valid_y))
-    predict_y = ridge.predict(valid_x)
-    ax_initial[0].plot(valid_x, predict_y, label = 'gamma = ' + str(gamma))
-ax_initial[0].plot(xeval, f(xeval), label = 'f(x) = 3x + 2')
-ax_initial[0].plot(valid_x, valid_y, '.', label = 'Validation Data')
-ax_initial[0].legend()
-ax_initial[0].set_title('Predicted lines for gamma = 0, 0.1')
-ax_initial[0].set_xlabel('x')
-ax_initial[0].set_ylabel('y')
-ax_initial[1].plot(gammas_initial, rss_initial)
-ax_initial[1].set_title('Residual Sum of Squares for gamma = 0, 0.1')
-ax_initial[1].set_xlabel('gamma')
-ax_initial[1].set_ylabel('Residual Sum of Squares')
-
+    ax_initial.plot(xeval, ridge.predict(xeval), label = 'gamma = ' + str(gamma))
+ax_initial.plot(xeval, f(xeval), label = 'f(x) = 3x + 2')
+ax_initial.legend()
 plt.show()
 
 
-gammas_log = [0, 0.1, 1, 10, 100]
+gammas_log = np.linspace(0,1000, 10000)
 fig_log10, ax_log10 = plt.subplots(1,1)
 rss_log10 = []
 for gamma in gammas_log:
@@ -55,7 +44,7 @@ ax_log10.set_xlabel('gamma')
 ax_log10.set_ylabel('Residual Sum of Squares')
 plt.show()
 
-gammas_fine = np.linspace(0,10,100)
+gammas_fine = np.linspace(0,10,1000)
 fig_fine, ax_fine = plt.subplots(1,1)
 rss_fine = []
 for gamma in gammas_fine:
@@ -67,3 +56,5 @@ ax_fine.set_title('Residual Sum of Squares for various gammas')
 ax_fine.set_xlabel('gamma')
 ax_fine.set_ylabel('Residual Sum of Squares')
 plt.show()
+
+print('The best gamma is ' + str(gammas_fine[np.argmin(rss_fine)]))
--- Code/ridge_2b_driver.py
+++ Code/ridge_2b_driver.py
@@ -14,32 +14,21 @@ seed = 50
 #get random sample, and divide into training and validation data
 train_x, train_y, valid_x, valid_y = random_sample(number_of_samples, f, a, b, number_of_train_samples, seed = seed)
 degree = 5
-gammas_initial = [0,0.1]
-
-fig_initial, ax_initial = plt.subplots(1,2)
-rss_initial = []
 xeval = np.linspace(-5,5,1000)
+gammas_initial = [0,0.1]
+fig_initial, ax_initial = plt.subplots(1,1)
+rss_initial = []
 for gamma in gammas_initial:
     ridge = estimators.ridge(gamma, degree)
     ridge.fit(train_x, train_y)
     rss_initial.append(ridge.RSS(valid_x, valid_y))
-    predict_y = ridge.predict(valid_x)
-    ax_initial[0].plot(valid_x, predict_y, label = 'gamma = ' + str(gamma))
-ax_initial[0].plot(xeval, f(xeval), label = 'f(x) = 3x + 2')
-ax_initial[0].plot(valid_x, valid_y, '.', label = 'Validation Data')
-ax_initial[0].legend()
-ax_initial[0].set_title('Predicted lines for gamma = 0, 0.1')
-ax_initial[0].set_xlabel('x')
-ax_initial[0].set_ylabel('y')
-ax_initial[1].plot(gammas_initial, rss_initial)
-ax_initial[1].set_title('Residual Sum of Squares for gamma = 0, 0.1')
-ax_initial[1].set_xlabel('gamma')
-ax_initial[1].set_ylabel('Residual Sum of Squares')
-
+    ax_initial.plot(xeval, ridge.predict(xeval), label = 'gamma = ' + str(gamma))
+ax_initial.plot(xeval, f(xeval), label = 'f(x) = x^2')
+ax_initial.legend()
 plt.show()
 
 
-gammas_log = [0, 0.1, 1, 10, 100]
+gammas_log = np.linspace(0,10000, 10000)
 fig_log10, ax_log10 = plt.subplots(1,1)
 rss_log10 = []
 for gamma in gammas_log:
@@ -53,7 +42,7 @@ ax_log10.set_xlabel('gamma')
 ax_log10.set_ylabel('Residual Sum of Squares')
 plt.show()
 
-gammas_fine = np.linspace(0,10,100)
+gammas_fine = np.linspace(0,10,1000)
 fig_fine, ax_fine = plt.subplots(1,1)
 rss_fine = []
 for gamma in gammas_fine:
@@ -65,3 +54,10 @@ ax_fine.set_title('Residual Sum of Squares for various gammas')
 ax_fine.set_xlabel('gamma')
 ax_fine.set_ylabel('Residual Sum of Squares')
 plt.show()
+
+
+
+print('The best gamma is ' + str(gammas_fine[np.argmin(rss_fine)]))
+
+
+
